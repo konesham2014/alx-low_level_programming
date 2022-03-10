42008ead23a52ca68ee0feaae7762fcf2a1b4134
Senait Mekonen <konesham1993@gmail.com>
2022-03-10 12:06:51 -0800

    Advanced questions

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - write to stdout
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(a, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
