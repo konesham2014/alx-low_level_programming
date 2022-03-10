461aa83fa0726b012ce56dc6707517c2a83d6a1f
Senait Mekonen <konesham1993@gmail.com>
2022-03-10 11:01:27 -0800

    3, 4 and 5 questions

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+	printf("");
+	printf("");
+	printf("");
+	printf("");
+	printf("");
+return(0);
+}
