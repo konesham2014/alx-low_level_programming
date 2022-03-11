c0f2d2b645a7854124a967de6d62a7287c7fdd58
Senait Mekonen <konesham1993@gmail.com>
2022-03-11 13:27:01 -0800

    all questions

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 49; n <= 57; n++)
+		putchar(n);
+	putchar(10);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include  <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
+}
